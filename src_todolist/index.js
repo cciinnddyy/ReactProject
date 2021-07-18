a7223450ec9cfe8aab80efd53fe100a557ead6cc
cindy <hsuan@cindy123.local>
2021-07-18 22:19:25 +1000

    todolist finish

---
+++ src_todolist/index.js
@@ -0,0 +1,12 @@
+//引入react 核心庫
+import React,{Component} from 'react';
+//引入 react DOM
+import ReactDOM from 'react-dom';
+// 引入 APP component
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+

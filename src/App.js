a659f9ef19f69f41a00c68be15864027414ef4eb
cindy <hsuan@cindy123.local>
2021-07-19 23:01:03 +1000

    user page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React,{Component} from 'react'
-import './App.css'
-import axios from 'axios' 
-class App extends Component{
-  
-  //初始化狀態
-  getStudents = ()=>{
-    axios.get("http://localhost:3000/api1/students").then(
-      response=>{console.log("success",response.data)},
-      error => {console.log("error",error)}
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        
+      </div>
     )
-  }
-  getCars = ()=>{
-    axios.get("http://localhost:3000/api2/cars").then(
-      response=>{console.log("success",response.data)},
-      error => {console.log("error",error)}
-    )
-  }
-  render(){
-    return (  
-          <div>
-            <button onClick={this.getStudents}>Click Me</button>
-            <button onClick={this.getCars}>Cars</button>
-          </div>
-      )
   }
 }
 
-//Export APP component
-export default App;
 

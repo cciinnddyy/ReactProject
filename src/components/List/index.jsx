660e86f97fd0455d8e5ea0b219d509eb2795b574
cindy <hsuan@cindy123.local>
2021-07-25 21:32:32 +1000

    pubsub

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import './index.css'
+import PubSub from 'pubsub-js'
 export default class List extends Component {
+    
+    state = {
+        data:[],
+        initiate:true,
+        isLoading:false,
+        errMsg:''
+    }
+
+    componentDidMount(){
+        this.token = PubSub.subscribe('updateState',function(msg,stateObj){
+            console.log(msg)
+            console.log('onSearch Trigger')
+            this.setState(stateObj)
+        }.bind(this))
+    }
+
+    componentWillUnmount(){
+        PubSub.unsubscribe(this.token)
+    }
+
     render() {
-        console.log(this.props)
+        console.log(this.state)
         return (
             <div className="row">
                 {
-                    this.props.initiate ? <h2>Hello</h2> :
-                    this.props.isLoading ? <h2>Loading....</h2>:
-                    this.props.errMsg ? <h2>{this.props.errMsg}</h2> :
-                        this.props.data.map((obj)=>{
+                    this.state.initiate ? <h2>Hello</h2> :
+                    this.state.isLoading ? <h2>Loading....</h2>:
+                    this.state.errMsg ? <h2>{this.state.errMsg}</h2> :
+                        this.state.data.map((obj)=>{
                             return <div key={obj.id} className="card">
                                     <a rel="noreferrer" href={obj.html_url} target="_blank">
                                     <img alt="Avatar"  src={obj.avatar_url} style={{width: '100px'}}/>
--- src_gitSearch_Ajax/App.js
+++ src_gitSearch_Ajax/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import SearchBar from './components/SearchBar';
 import List from './components/List'
 export default class App extends Component {
+  
   state = {
     data:[],
     initiate:true,

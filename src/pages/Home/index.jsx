1831d9b8ab9605478c72fe1e4f9c3bdca5efeebe
cciinnddyy <cciinnddyy0033@gmail.com>
2021-09-28 23:00:11 +1000

    News Messages

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
-
+import News from './News'
+import Messages from './Messages'
 export default class Home extends Component {
     render() {
         return (
-            <h3>我是Home的内容</h3>
+            <div>
+            <h2>Home组件内容</h2>
+            <div>
+              <ul class="nav nav-tabs">
+                <li>
+                  <a class="list-group-item" href="./home-news.html">News</a>
+                </li>
+                <li>
+                  <a class="list-group-item active" href="./home-message.html">Message</a>
+                </li>
+              </ul>
+                <News/>
+                <Messages/>
+            </div>
+          </div>
         )
     }
 }
